96365b3ab7ed255ced2b679b8c459de6c4ffabb1
JayJames <jayjameshlw@gmail.com>
2021-10-05 04:04:36 +0000

    dd

--- software/yay-software.sh
+++ software/yay-software.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yay -S jmtpfs bash-completion  imagewriter vscodium-bin  gamepad-tool-bin gitkraken
+yay -S jmtpfs bash-completion imagewriter vscodium-bin gamepad-tool-bin gitkraken mangohud
