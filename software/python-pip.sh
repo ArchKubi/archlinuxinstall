3088838fac795f2bddf6ac8c89b660151dc3f4bd
JayJames <jayjameshlw@gmail.com>
2021-10-06 13:10:57 +0000

    dd

--- software/python-pip.sh
+++ software/python-pip.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 yay -S python-pip 
-python3 -m pip install psutil python-magic pyinstaller cairocffi cffi xcffib
-#PyInstaller tutorial1.py --onefile 
+python3 -m pip install psutil python-magic pyinstaller  cairocffi cffi xcffib 
+
+
+#PyInstaller tutorial1.py --onefile
