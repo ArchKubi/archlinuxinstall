5df1da54be7863878ab8bfd6d055d9a8faa54a68
JayJames <jayjameshlw@gmail.com>
2021-10-13 21:13:07 +0000

    dd

--- software/extra-software.sh
+++ software/extra-software.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo pacman -S exfat-utils fuse-exfat a52dec faac faad2 flac jasper lame libdca libdv gst-libav libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore  libdvdcss libdvdread libdvdnav  dvd+rw-tools dvdauthor dvgrab 
-sudo pacman -S xarchiver zip unzip mtpfs gvfs-mtp fakeroot jshon expac git wget htop lxappearance neofetch nitrogen xfce4-terminal qbittorrent
-sudo pacman -S variety pavucontrol xfce4-appfinder xfce4-taskmanager thunar rofi scrot vlc ristretto net-tools p7zip unrar gparted conky jdk-openjdk
+sudo pacman -S xarchiver zip unzip mtpfs gvfs-mtp jshon expac git  htop lxappearance neofetch nitrogen xfce4-terminal qbittorrent
+sudo pacman -S variety xfce4-appfinder xfce4-taskmanager thunar rofi scrot vlc ristretto net-tools p7zip unrar gparted conky jdk-openjdk
 
 echo sudo pacman -S amd-ucode or 
 sudo pacman -S intel-ucode
