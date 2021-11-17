b922a50b3ac5f1dc3cc91cf0680d4d0c3b416dcd
ArchKubi <80429360+ArchKubi@users.noreply.github.com>
2021-11-17 00:27:20 +0000

    dd

--- fix_problem/time_problem.sh
+++ fix_problem/time_problem.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #example "2021-11-10 14:14:00"
-timedatectl set-time "2021-11-10 14:14:00"
+timedatectl set-time "2021-11-16 18:28:00"
 sudo su
 timedatectl set-local-rtc 0
 hwclock --systohc --utc
