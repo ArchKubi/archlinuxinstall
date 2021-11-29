b39137d5fc1f490b9042ef1463ddc24d06752567
ArchKubi <80429360+ArchKubi@users.noreply.github.com>
2021-11-30 00:14:47 +0300

    dd

--- Shell/shells.sh
+++ Shell/shells.sh
@@ -13,8 +13,6 @@ do
             sudo pacman -S fish
             curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
             chsh -s $(which fish)
-            omf update
-            omf install bira
             break
             ;;
 
