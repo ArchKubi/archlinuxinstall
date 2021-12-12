8d08c2d73487e3cc6b73f96ffda729e9a61db81a
ArchKubi <80429360+ArchKubi@users.noreply.github.com>
2021-12-12 11:14:10 +0000

    dd

--- desktop_manager/high_resolution/Pink_qtile/display.sh
+++
@@ -1,4 +0,0 @@
-#xrandr --newmode "CUSTOM" 109.00  1280 1368 1496 1712  1024 1027 1034 1063 -hsync +vsync
-xrandr --newmode "CUSTOM" 107.75  1280 1368 1496 1712  1012 1015 1025 1050 -hsync +vsync
-xrandr --addmode VGA-1 "CUSTOM"
-xrandr --output VGA-1 --mode "CUSTOM"
