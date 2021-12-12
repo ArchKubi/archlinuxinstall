802eb85a75dc35a5fb4a495a792183b504e00146
ArchKubi <80429360+ArchKubi@users.noreply.github.com>
2021-12-12 10:45:31 +0000

    dd

--- format/English_/part2.sh
+++ format/English_/part2.sh
@@ -48,7 +48,7 @@ echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-read -p "Enter Keymap |KEYMAP=trq|---|KEYMAP=us| enter like this: "
+echo "Keymap |KEYMAP=trq|---|KEYMAP=us|"
 touch /etc/vconsole.conf
 echo KEYMAP=trq >> /etc/vconsole.conf 
 
