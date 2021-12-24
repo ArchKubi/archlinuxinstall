919620f43c8ee5cf351f6df9b84b3fdc0011fdca
archkubi <jayjameshlw@gmail.com>
2021-12-24 16:16:29 +0000

    dd

--- software/extra-software.sh
+++ software/extra-software.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S exfat-utils fuse-exfat a52dec faac faad2 jasper lame libdca libdv gst-libav libmad libmpeg2 libtheora wavpack x264 xvidcore  libdvdcss libdvdread libdvdnav  dvd+rw-tools dvdauthor dvgrab 
 sudo pacman -S xarchiver zip unzip mtpfs gvfs-mtp jshon expac htop lxappearance neofetch nitrogen cool-retro-term qbittorrent leafpad 
-sudo pacman -S variety rofi scrot vlc ristretto net-tools p7zip unrar gparted conky jdk-openjdk emacs
+sudo pacman -S scrot vlc ristretto net-tools p7zip unrar gparted jdk-openjdk emacs
 
 
 PS3='Please enter your choice: '
@@ -38,13 +38,13 @@ read -rsn1 -p "install your browser firefox is default //Press Enter" variable; 
 sudo pacman -S 
 
 PS3='Please enter your choice: '
-options=("Firefox" "Vivaldi" "Opera" "Chromium" "Quit")
+options=("QuteBrowser" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Firefox")
-        echo "you chose Firefox"
-            sudo pacman -S firefox
+        "QuteBrowser")
+        echo "you chose QuteBrowser"
+            sudo pacman -S qutebrowser
             break
             ;;
 
