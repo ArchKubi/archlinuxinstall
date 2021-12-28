7a787da684a6088b96bb1d4d3b254c5535b454f9
archkubi <jayjameshlw@gmail.com>
2021-12-29 00:44:18 +0300

    uwu

--- installer/multi_lang/gui_version/part2-test.sh
+++ installer/multi_lang/gui_version/part2-test.sh
@@ -83,19 +83,6 @@ advancedMenu
 echo "Your language is OK!  //Press Enter" variable; echo
 ################################################################################################################
 
-################################################################################################################
-timedatectl list-timezones
-read -rsn1 -p "Dont Forget Time Zone"
-
-        timeZone = $(whiptail --inputbox "Enter Time Zone" 10 30 3>&1 1>&2 2>&3)
-            echo "$timeZone" >> /etc/hostname
-            timedatectl set-timezone $timezone
-            timedatectl set-local-rtc 0
-            timedatectl set-ntp true 
-            read -rsnl -p "if you see your time zone NICE! if you not report please"
-            read -rsn1 -p "Timezone Completed! //Press Enter" variable; echo
-            locale-gen
-################################################################################################################
 
 
     else
